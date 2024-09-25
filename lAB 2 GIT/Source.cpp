cc88409ffc8776902fbf8de3720c8770880a6e10
IZA <rg76g@yandex.ru>
2024-09-25 21:55:46 +0300

    версия 2

--- lAB 2 GIT/Source.cpp
+++ lAB 2 GIT/Source.cpp
@@ -6,20 +6,91 @@ void printGreeting(const std::string& name, int age) {
     std::cout << "Привет, " << name << "! Вам " << age << " лет." << std::endl;
 }
 
+// Функция для вывода меню
+void printMenu() {
+    std::cout << "Меню:" << std::endl;
+    std::cout << "1. Ввести имя и возраст" << std::endl;
+    std::cout << "2. Вывести приветственное сообщение" << std::endl;
+    std::cout << "3. Изменить имя" << std::endl;
+    std::cout << "4. Изменить возраст" << std::endl;
+    std::cout << "5. Выход" << std::endl;
+}
+
+// Функция для изменения имени
+void changeName(std::string& name) {
+    std::cout << "Введите новое имя: ";
+    std::cin >> name;
+}
+
+// Функция для изменения возраста
+void changeAge(int& age) {
+    std::cout << "Введите новый возраст: ";
+    std::cin >> age;
+}
+
 int main() {
     setlocale(LC_ALL, "Ru");
     // Переменные для хранения имени и возраста
     std::string name;
     int age;
 
-    // Ввод имени и возраста
-    std::cout << "Введите ваше имя: ";
-    std::cin >> name;
-    std::cout << "Введите ваш возраст: ";
-    std::cin >> age;
+    int choice;
 
-    // Вывод приветственного сообщения
-    printGreeting(name, age);
+    // Основной цикл программы
+    while (true) {
+        printMenu();
+        std::cout << "Введите ваш выбор: ";
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            // Ввод имени и возраста
+            std::cout << "Введите ваше имя: ";
+            std::cin >> name;
+            std::cout << "Введите ваш возраст: ";
+            std::cin >> age;
+            break;
+        }
+        case 2: {
+            // Вывод приветственного сообщения
+            if (!name.empty()) {
+                printGreeting(name, age);
+            }
+            else {
+                std::cout << "Введите имя и возраст перед выводом приветственного сообщения." << std::endl;
+            }
+            break;
+        }
+        case 3: {
+            // Изменить имя
+            if (!name.empty()) {
+                changeName(name);
+            }
+            else {
+                std::cout << "Введите имя перед изменением." << std::endl;
+            }
+            break;
+        }
+        case 4: {
+            // Изменить возраст
+            if (age != 0) {
+                changeAge(age);
+            }
+            else {
+                std::cout << "Введите возраст перед изменением." << std::endl;
+            }
+            break;
+        }
+        case 5: {
+            // Выход
+            std::cout << "До свидания!" << std::endl;
+            return 0;
+        }
+        default: {
+            std::cout << "Неправильный выбор. Попробуйте снова." << std::endl;
+        }
+        }
+    }
 
     return 0;
 }
